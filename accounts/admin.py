dea123ee9cf0827f1101fbe50c34c39f1355e7e6
JooJooda <liyingzhou375@gmail.com>
2024-07-22 23:11:34 +0900

    feat : admin 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class UserAdmin(UserAdmin):
+
+    list_display = (
+        'username',
+        'email',
+    )
+
+    list_display_links = (
+        'username',
+        'email',
+    )
+
+admin.site.register(User, UserAdmin)
