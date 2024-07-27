b6049d3547252c67696a4060b18190c9cb17d2cf
Ubuntu <ubuntu@ip-172-31-13-107.ap-northeast-2.compute.internal>
2024-07-27 13:56:31 +0000

    fix : migration 오류 수정

--- likelion_hackathon/settings.py
+++ likelion_hackathon/settings.py
@@ -48,7 +48,7 @@ ALLOWED_HOSTS = ['43.201.215.123', '127.0.0.1', 'localhost', 'develop--billigo-t
 # Application definition
 
 DJANGO_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
