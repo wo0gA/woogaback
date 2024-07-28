ad7003b716267800a32123586b344d3b97fa138a
Temple2001 <kimarang0@naver.com>
2024-07-28 20:44:00 +0900

    Revert "chore: settings.py 수정"
    This reverts commit 6a5c478656cc33ba9c60c2053a7c9b3b7e6fde80.

--- likelion_hackathon/settings.py
+++ likelion_hackathon/settings.py
@@ -104,8 +104,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://develop--billigo-test.netlify.app",
-    "https://server.templ.es",
+    "https://develop--billigo-test.netlify.app"
 ]
 
 ROOT_URLCONF = 'likelion_hackathon.urls'
