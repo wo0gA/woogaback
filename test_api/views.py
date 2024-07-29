6ae6daf7ce828ce740156552f18ac2ae3e5b79d3
Temple2001 <kimarang0@naver.com>
2024-07-29 22:39:31 +0900

    feat: auto deploy 설정 및 테스트

--- test_api/views.py
+++ test_api/views.py
@@ -6,5 +6,5 @@ def hello_world(request):
     if request.method == "GET":
         return JsonResponse({
             'status' : 200,
-            'data' : "Hello World!!!"
+            'data' : "Hello World!!!!"
         }, status=200)
