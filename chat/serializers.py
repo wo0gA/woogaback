eded6ffea6f0238bed090f451b4c833e3b9859fb
Temple2001 <kimarang0@naver.com>
2024-08-02 17:19:35 +0900

    fix: chat response 수정

--- chat/serializers.py
+++ chat/serializers.py
@@ -20,7 +20,7 @@ class ChatRoomSerializer(serializers.ModelSerializer):
     # SerializerMethodField는 ChatRoom 필드에 더해 추가적으로 반환되는 필드들이다
     # read-only라서 저장은 되지 않는다 (.save()에 안쓰임)
     latest_message = serializers.SerializerMethodField()
-    opponent_email = serializers.SerializerMethodField()
+    opponent_username = serializers.SerializerMethodField()
     shop_user = SimpleUserSerializer(read_only=True)
     visitor_user = SimpleUserSerializer(read_only=True)
     messages = MessageSerializer(many=True, read_only=True, source='messages.all')
@@ -28,7 +28,7 @@ class ChatRoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ChatRoom
-        fields = ('id', 'shop_user', 'visitor_user', 'latest_message', 'opponent_email', 'messages', 'product')
+        fields = ('id', 'shop_user', 'visitor_user', 'latest_message', 'opponent_username', 'messages', 'product')
 
     # get_<method_field> 메서드 파라미터 안에 있는 obj는 ChatRoom 객체이다
 
@@ -40,15 +40,15 @@ class ChatRoomSerializer(serializers.ModelSerializer):
         else:
             return None
 
-    def get_opponent_email(self, obj):
-        # 요청 사용자와 대화하는 상대방의 이메일 가져오기
+    def get_opponent_username(self, obj):
+        # 요청 사용자와 대화하는 상대방의 닉네임 가져오기
         request_user_email = self.context['request'].query_params.get('email', None)
         if request_user_email is None:
             return None
         if request_user_email == obj.shop_user.email:
-            return obj.visitor_user.email
+            return obj.visitor_user.username
         else:
-            return obj.shop_user.email
+            return obj.shop_user.username
 
     # def get_shop_user_username(self, obj):
     #     return obj.shop_user.username
