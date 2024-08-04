68200b4cfa8ac755202b8973fcd15945b1dbe17a
JooJooda <liyingzhou375@gmail.com>
2024-08-04 22:22:50 +0900

    chore : 수정사항 서버에 반영

--- products/serializers.py
+++ products/serializers.py
@@ -40,7 +40,7 @@ class SimpleCategorySerializer(serializers.ModelSerializer):
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ['hashtag', 'views']
 
 
 # product 관련 시리얼라이저
@@ -52,71 +52,104 @@ class ProductThumbnailSerializer(serializers.ModelSerializer):
 
 class ProductSerializerForWrite(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
-    thumbnails = serializers.SerializerMethodField()
+    thumbnails = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
     
     class Meta:
         model = Product
         exclude = ['views']
 
-    def get_thumbnails(self, obj):
-        thumbnails = obj.thumbnails.all()
-        return ProductThumbnailSerializer(thumbnails, many=True).data
 
     def create(self, validated_data):
         tags_data = validated_data.pop('tags', [])
-        
+        print('tags_data',tags_data)
+
         thumbnails_data = validated_data.pop('thumbnails', [])
+        
+        tags_inital_data = self.initial_data.get('tags', [])
 
         product = Product.objects.create(**validated_data)
 
         for tag_data in tags_data:
-            tag, created = Tag.objects.get_or_create(**tag_data)
+            tag, created = Tag.objects.get_or_create(hashtag=tag_data['hashtag'])
             product.tags.add(tag)
         
         for thumbnail_data in thumbnails_data:
             ProductThumbnail.objects.create(product=product, thumbnail=thumbnail_data)
 
         return product
+# class ProductSerializerForWrite(serializers.ModelSerializer):
+#     tags = TagSerializer(many=True, required=False)
+#     thumbnails = serializers.SerializerMethodField()
     
-    # count 성능 보완 필요
-    def update(self, instance, validated_data):
-        import json
-        tags_data = validated_data.pop('tags', [])
-        thumbnails_data = validated_data.pop('thumbnails', [])
+#     class Meta:
+#         model = Product
+#         exclude = ['views']
+
+#     def get_thumbnails(self, obj):
+#         thumbnails = obj.thumbnails.all()
+#         return ProductThumbnailSerializer(thumbnails, many=True).data
+
+#     def create(self, validated_data):
+#         tags_data = validated_data.pop('tags', [])
+#         print(tags_data)
+#         thumbnails_data = validated_data.pop('thumbnails', [])
+#         print(thumbnails_data)
+
+#         product = Product.objects.create(**validated_data)
+
+#         for tag_data in tags_data:
+#             print(tag_data)
+#             tag, created = Tag.objects.get_or_create(**tag_data)
+
+#             product.tags.add(tag)
         
-        # 일반 필드 업데이트
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+#         for thumbnail_data in thumbnails_data:
+#             thumbnail = ProductThumbnail.objects.create(product=product, thumbnail=thumbnail_data)
+#             product.thumbnails.add(thumbnail)
+
+#         return product
+    
+#     # count 성능 보완 필요
+#     def update(self, instance, validated_data):
+#         import json
+#         tags_data = validated_data.pop('tags', [])
+#         thumbnails_data = validated_data.pop('thumbnails', [])
         
-        # 현재 태그와 새 태그 처리
-        current_tags = set(instance.tags.all())
-        new_tags = set()
+#         # 일반 필드 업데이트
+#         for attr, value in validated_data.items():
+#             setattr(instance, attr, value)
+        
+#         # 현재 태그와 새 태그 처리
+#         current_tags = set(instance.tags.all())
+#         new_tags = set()
 
-        # 새 태그 데이터 처리
-        for tag_data in tags_data:
-            tag, created = Tag.objects.get_or_create(**tag_data)
-            new_tags.add(tag)
+#         # 새 태그 데이터 처리
+#         for tag_data in tags_data:
+#             tag, created = Tag.objects.get_or_create(**tag_data)
+#             new_tags.add(tag)
 
-        # 태그 삭제
-        tags_to_remove = current_tags - new_tags
-        for tag in tags_to_remove:
-            # 현재 제품과 연결이 없는 다른 제품이 존재하는지 확인
-            if tag.products.count() == 1:  # 현재 제품과만 연결된 경우
-                tag.delete()  # 태그 삭제
+#         # 태그 삭제
+#         tags_to_remove = current_tags - new_tags
+#         for tag in tags_to_remove:
+#             # 현재 제품과 연결이 없는 다른 제품이 존재하는지 확인
+#             if tag.products.count() == 1:  # 현재 제품과만 연결된 경우
+#                 tag.delete()  # 태그 삭제
 
-        # 새로운 태그 설정
-        instance.tags.set(new_tags)
+#         # 새로운 태그 설정
+#         instance.tags.set(new_tags)
 
-        # 현재 썸네일 삭제
-        instance.thumbnails.all().delete()
+#         # 현재 썸네일 삭제
+#         instance.thumbnails.all().delete()
 
-         # 새 썸네일 추가
-        for thumbnail_data in thumbnails_data:
-            ProductThumbnail.objects.create(product=instance, **thumbnail_data)
+#          # 새 썸네일 추가
+#         for thumbnail_data in thumbnails_data:
+#             ProductThumbnail.objects.create(product=instance, **thumbnail_data)
 
-        # 제품 저장
-        instance.save()
-        return instance
+#         # 제품 저장
+#         instance.save()
+#         return instance
     
     
 class SimpleUserSerializer(serializers.ModelSerializer):
